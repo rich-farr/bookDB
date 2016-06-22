6399239233c7f744fa60d92dbcec687bb92a9846
rich-farr <scarletmanuka@gmail.com>
2016-06-22 15:20:08 +1200

    Change npm start to nodemon, alter template

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Rich's book catalogue" });
 });
 
 module.exports = router;
