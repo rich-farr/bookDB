267dedd38dffb4d732fcd44da801db5dd119d241
rich-farr <scarletmanuka@gmail.com>
2016-06-26 14:46:07 +1200

    Create seed files for addBooks & addAuthors

--- seeds/addAuthors.js
+++ seeds/addAuthors.js
@@ -1,13 +1,11 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
-      ]);
-    });
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('authors').del(),
+
+    // Inserts seed entries
+    knex('authors').insert({id: 1, first_name: 'Jack', last_name: 'Kerouac'}),
+    knex('authors').insert({id: 2, first_name: 'Hanif', last_name: 'Kureishi'}),
+    knex('authors').insert({id: 3, first_name: 'F. Scott', last_name: 'Fitzgerald'})
+  );
 };
--- seeds/addBooks.js
+++ seeds/addBooks.js
@@ -1,13 +1,12 @@
-
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
-      ]);
-    });
+  return Promise.join(
+    // Deletes ALL existing entries
+    knex('books').del(),
+
+    // Inserts seed entries
+    knex('books').insert({id: 1, title: 'The Great Gatsby', year: 1925, image_link: '"http://www.beautifulbookcovers.com/wp-content/uploads/2013/01/gatsby-original-cover-art.jpg"', have_read: true, author_id: 3}),
+    knex('books').insert({id: 2, title: 'On the Road', year: 1957, image_link: '"https://s-media-cache-ak0.pinimg.com/736x/16/d5/e3/16d5e3ddfd15328412f96366e742c3f5.jpg"', have_read: true, author_id: 1}),
+    knex('books').insert({id: 3, title: 'The Buddha of Suburbia', year: 1990, image_link: 'http://www.gutenberg-gym.de/redaktion/_data/cover1.jpg', have_read: false, author_id: 2})
+  );
 };
+
