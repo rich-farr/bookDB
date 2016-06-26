1ced62b34d9b6d6d0e15d94ad24e7ab0da28a765
rich-farr <scarletmanuka@gmail.com>
2016-06-26 14:33:58 +1200

    Add image column to createBooks migration file

--- migrations/20160626140556_createBooksTable.js
+++ migrations/20160626140556_createBooksTable.js
@@ -5,6 +5,7 @@ exports.up = function(knex, Promise) {
     table.increments('id').primary
     table.string('title')
     table.integer('year')
+    table.string('image_link')
     table.boolean('have_read')
     table.integer('author_id')
   })
